d7f2f41ab24182316b0d9229664a6246a0bdb677
Dan <dan_paris@wycliffeassociates.org>
2020-07-21 15:47:07 -0400

    fix appname

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Your fat/shadow jar should be in the same dir as build.sh 
 # Modify these vars as desired
-export APPNAME=BTTConverter
+export APPNAME=MyJar2AppImage
 export VERSION=1.0
 export ARCH=x86_64
 
